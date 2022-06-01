ae4f6a0187227486f8a305eb2bd859e8d5521905
maurisplasse <maurisplasse@gmail.com>
2022-05-31 23:02:24 -0300

    update

---
+++ exercicios/ex008&009.js
@@ -0,0 +1,18 @@
+var vel = 60
+console.log(`Você está andando a ${vel} km/h`)
+if (vel > 70) {
+    console.log('Você ultrapassou a velocidade permitida! Você foi <strong>MULTADO!</strong>')
+}
+else {
+    console.log('Você fez mais do que a sua obrigação!')
+}
+
+
+var pais = 'Líbia'
+console.log(`Reside em ${pais}`)
+
+if (pais == 'Brasil') {
+    console.log('Você é brasileiro!')
+} else {
+    console.log('Você é turista!')
+}
